6bcb82f74129d37d35e56245f183723e63c81d0f
LFFavoretto <favorettof14@gmail.com>
2026-05-01 18:35:09 -0300

    Adicionado o arquivo database.sql com estrutura do banco

---
+++ database/sql/database.sql
@@ -0,0 +1,42 @@
+-- Criação de tabelas 
+CREATE TABLE unidade_saude (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(150) NOT NULL,
+logradouro VARCHAR(100) NOT NULL,
+bairro VARCHAR(80) NOT NULL,
+numero INT NOT NULL);
+
+CREATE TABLE insumos (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+marca VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE usuarios (
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_unidade_saude INT NOT NULL,
+nome VARCHAR(100) NOT NULL,
+email VARCHAR(150) NOT NULL UNIQUE,
+senha_hash VARCHAR(255) NOT NULL,
+tipo_usuario ENUM('administrador', 'operador') NOT NULL,
+FOREIGN KEY(id_unidade_saude) REFERENCES unidade_saude(id)
+);
+
+CREATE TABLE estoque (
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_unidade_saude INT NOT NULL,
+id_insumos INT NOT NULL,
+id_usuarios INT NOT NULL,
+tipo_movimentacao ENUM('entrada', 'saida') NOT NULL,
+data_movimentacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+quantidade INT NOT NULL,
+lote VARCHAR(20) NOT NULL,
+data_validade DATE NOT NULL,
+observacoes TEXT,
+FOREIGN KEY(id_unidade_saude) REFERENCES unidade_saude(id),
+FOREIGN KEY(id_insumos) REFERENCES insumos(id),
+FOREIGN KEY(id_usuarios) REFERENCES usuarios(id)
+);
+
+-- Atualização da tabela usuarios
+ALTER TABLE usuarios ADD ativo BOOLEAN DEFAULT TRUE;
