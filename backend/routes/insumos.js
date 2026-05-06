5321e23041a411ac5a9faabb7898950ccce71a5a
lianccn <lianmarinheiro2005@gmail.com>
2026-05-05 21:57:35 -0300

    update routes

--- backend/routes/insumos.js
+++ backend/routes/insumos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+const {insumo} = required('models.js')
 
 
 // ================= GET =================
